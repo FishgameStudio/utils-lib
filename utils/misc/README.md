04623393819587e0e03c9c784561108e0c596b9d
Fishgame Studio <popxh@outlook.com>
2026-06-01 16:28:49 +0800

    Add misc

--- utils/misc/README.md
+++ utils/misc/README.md
@@ -1,14 +1,11 @@
+
 # MISC
-Here's some **miscellaneous** things from my **secret & awesome ideas**\.\.\.
-## Nevigation
-|        File         |              Usage                                |
-|---------------------|---------------------------------------------------|
-|test_accessibility.py|Check **accessibilities** of some websites.        |
-|many_sort.py         |Many sort algorithm functions.                     |
-|gen_sheet.py         |Generate **This sheet** automaticly.               |
-|||
-|||
-|||
-|||
-|||
-|||
+Here's some **miscellaneous** things from my **secret & awesome ideas**...
+
+## Navigation
+
+| File                  | Usage                                      |
+|-----------------------|--------------------------------------------|
+| gen_sheet.py          | Generate **This sheet** automaticly.       |
+| many_sort.py          | Many sort algorithm functions.             |
+| test_accessibility.py | Test **accessibilities** of some websites. |
--- utils/misc/gen_sheet.py
+++ utils/misc/gen_sheet.py
@@ -4,34 +4,53 @@
 import os
 
 def gen_sheet() -> str:
-    # Traverse files and read its introduction.
-    files = os.listdir()
+    files = os.listdir(os.path.dirname(__file__))
+    py_files = []
     introductions = []
+
     for file in files:
-        if not file.endswith(".py"): continue
-        text = open(file, "r", encoding="utf-8").read()
-        introduction = text.split("\n")[1][2:]
-        introductions.append(introduction)
-    file_max_len, intro_max_len = 0, 0
-    file_max_len = max(len(f) for f in files) if files else 10
+        if not file.endswith(".py"):
+            continue
+        
+        try:
+            text = open(os.path.dirname(__file__) + "/" + file, "r", encoding="utf-8").read()
+            lines = text.splitlines()
+            if len(lines) >= 2:
+                intro = lines[0].lstrip("# ").strip()
+            else:
+                intro = "No description"
+            
+            py_files.append(file)
+            introductions.append(intro)
+        except:
+            continue
+
+    file_max_len = max(len(f) for f in py_files) if py_files else 10
     intro_max_len = max(len(i) for i in introductions) if introductions else 20
+
     res = ""
-    res += f"|{"File".center(file_max_len)}|{"Usage".center(intro_max_len)}|\n"
-    res += f"|{"-" * file_max_len}|{"-" * intro_max_len}|\n"
-    for f in files:
-        for i in introductions:
-            res += "|" + f + " " * (file_max_len - len(f)) + "|" + i + " " * (intro_max_len - len(i))
+    res += f"| {'File'.ljust(file_max_len)} | {'Usage'.ljust(intro_max_len)} |\n"
+    res += f"|{'-'*(file_max_len+2)}|{'-'*(intro_max_len+2)}|\n"
+
+    for f, i in zip(py_files, introductions):
+        res += f"| {f.ljust(file_max_len)} | {i.ljust(intro_max_len)} |\n"
+
     return res
+
+
 def write_to_readme() -> None:
-    sheet = gen_sheet() 
+    sheet = gen_sheet()
     text = """
 # MISC
-Here's some **miscellaneous** things from my **secret & awesome ideas**\\.\\.\\.
-## Nevigation
-""" + sheet + "\n"
-    with open("README.md", "w", encoding="utf-8") as f:
+Here's some **miscellaneous** things from my **secret & awesome ideas**...
+
+## Navigation
+
+""" + sheet
+
+    with open(f"{os.path.dirname(__file__)}/README.md", "w", encoding="utf-8") as f:
         f.write(text)
+
 
 if __name__ == '__main__':
     write_to_readme()
-
